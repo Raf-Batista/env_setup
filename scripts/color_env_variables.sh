873da8f1124624f3cbc38f793a31c35dbc1d865e
Raf-Batista <RafaelB2424@gmail.com>
2020-10-09 13:14:39 -0700

    Create script to set color env variables

---
+++ scripts/color_env_variables.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# https://unix.stackexchange.com/questions/92563/friendly-terminal-color-names-in-shell-scripts
+
+################## color format variables #######################
+red=$'\e[1;31m'
+green=$'\e[1;32m'
+yellow=$'\e[1;33m'
+blue=$'\e[1;34m'
+magenta=$'\e[1;35m'
+cyan=$'\e[1;36m'
+end=$'\e[0m'
+
+export red 
+export green 
+export blue 
+export magenta 
+export cyan 
+export end
