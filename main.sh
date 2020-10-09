e423f5851ee08a2f4f09086de4e5f4628e14c03c
Raf-Batista <RafaelB2424@gmail.com>
2020-10-08 20:18:54 -0700

    Add option for Docker, Ruby, and Node

--- main.sh
+++ main.sh
@@ -4,7 +4,13 @@ while [ -n "$1" ]; do
 
 	case "$1" in
 
-	-r | --ruby) echo "install ruby" ;;
+	# -r | --ruby) echo "install ruby" ;; Update script before commenting this out
+
+	-n) ./scripts/node.sh ;;
+
+  # -g) ./scripts/github.sh ;; # Use Faraday to log a user in to GitHub before commenting this out
+
+	-d) ./scripts/docker.sh
 
     --gitbranch) ./scripts/display_git_branch.sh ;;
 
