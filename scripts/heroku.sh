c343f03ef94a6bcb46d419ebfa1e48a10f3facb8
Raf-Batista <RafaelB2424@gmail.com>
2020-10-09 13:15:48 -0700

    Create script to install Heroku

---
+++ scripts/heroku.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+######## Install Heroku to deploy apps ##########
+# sudo snap install --classic heroku
+
+xclip -sel clip < ~/.ssh/id_rsa.pub
+# Copies the contents of the id_rsa.pub file to your clipboard
+
+echo 'The SSH was copied to your clipboard. Enter the SSH Key on github'
+
+########## Install Postgresql ############
+
+sudo apt install postgresql postgresql-contrib
+
+# set role postgres with password postgres
+sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'postgres';"
+
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+
+# Install PGAdmin4
+# Install PostgreSQL key and add repository
+
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+
+# install PGAdmin repo
+sudo apt install pgadmin4 pgadmin4-apache2 -y
