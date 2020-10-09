ca79d14624203d6541ecc6bf7fd47e84461d9003
Raf-Batista <RafaelB2424@gmail.com>
2020-10-09 11:36:33 -0700

    Export color format variables to be used in the other scripts

--- main.sh
+++ main.sh
@@ -1,8 +1,22 @@
 #!/bin/bash 
 
+# Link to the format colors
+# https://unix.stackexchange.com/questions/92563/friendly-terminal-color-names-in-shell-scripts
+
 red=$'\e[1;31m'
 green=$'\e[1;32m'
+yellow=$'\e[1;33m'
+blue=$'\e[1;34m'
+magenta=$'\e[1;35m'
+cyan=$'\e[1;36m'
 end=$'\e[0m'
+
+export red 
+export green 
+export blue 
+export magenta 
+export cyan 
+export end
 
 while(($#)); do 
 
