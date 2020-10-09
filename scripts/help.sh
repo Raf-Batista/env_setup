dd643f26aeb070f6da56b2c18eb02845afa22006
Raf-Batista <RafaelB2424@gmail.com>
2020-10-09 13:15:31 -0700

    Create script to print out a help message with the available commands

--- scripts/help.sh
+++ scripts/help.sh
@@ -1,5 +1,7 @@
-!#/bin/bash 
+#!/bin/bash 
 
 printf "\n Here are the available commands" 
 printf "\n ${blue}--gitbranch${end} (show git branch in green)"
 printf "\n ${blue}-n${end} (install node and nvm)" 
+
+export test="hey"
