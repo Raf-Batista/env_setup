3f212df20fb3bf0a3315d4d42ca10b38372f3812
Raf-Batista <RafaelB2424@gmail.com>
2020-10-12 18:58:36 -0700

    Rename file to app.sh

---
+++ app.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+
+source ./scripts/color_env_variables.sh
+
+while : ; do 
+	printf "\n Welcome to env setup" 
+	printf "\n Type exit to exit the program \n"
+
+	read input 
+
+	if [[ $input == "exit" ]]; then  
+		echo "Goodbye"
+		break
+	fi 
+
+
+	./scripts/main.sh $input
+
+done 
