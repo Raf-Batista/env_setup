8f811890b2129707a37e12639e6f61bc0c03245e
Raf-Batista <RafaelB2424@gmail.com>
2020-10-12 18:58:14 -0700

    Move the logic to set up the dev environment to a separate file

---
+++ scripts/main.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+while(($#)); do 
+
+	case "$1" in
+
+	# -r | --ruby) echo "install ruby" ;; Update script before commenting this out
+
+	-n) ./scripts/node.sh ;;
+
+  # -g) ./scripts/github.sh ;; # Use Faraday to log a user in to GitHub before commenting this out
+
+	-d) ./scripts/docker.sh ;;
+
+    --gitbranch) ./scripts/display_git_branch.sh ;;
+
+    --help) ./scripts/help.sh ;;
+
+	*) source ./scripts/help.sh ;;
+
+	esac
+
+	shift
+
+done
