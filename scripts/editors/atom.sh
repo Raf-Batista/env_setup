939a1117fbbc55d57625d72ddc1b090306a75d08
Raf-Batista <RafaelB2424@gmail.com>
2020-10-12 19:30:51 -0700

    Add script to install Atom

---
+++ scripts/editors/atom.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+
+# Install Atom
+
+sudo apt-get install atom
+
+# https://nearsoft.com/blog/how-to-install-packages-in-atom/
+
+# Install Atom packages
+
+# apm install atom-live-server
+# apm install atom-runner
+# apm install busy-signal
+# apm install color-picker
+# apm install emmet
+# apm install intentions
+# apm install language-babel
+# apm install linter
+# apm install linter-eslint
+# apm install linter-ruby
+# apm install linter-ui-default
+# apm install minimap
+# apm install pigments
+# apm install turbo-javascript
