06d3d349715d873f997ed7203a08db86764a003e
Rafael Batista <RafaelB2424@gmail.com>
2020-10-14 11:56:19 -0400

    Remove exported variable

--- scripts/help.sh
+++ scripts/help.sh
@@ -3,5 +3,3 @@
 printf "\n Here are the available commands" 
 printf "\n ${blue}--gitbranch${end} (show git branch in green)"
 printf "\n ${blue}-n${end} (install node and nvm)" 
-
-export test="hey"
