b4982fc9251a480312757c094c92511705e5598c
Raf-Batista <RafaelB2424@gmail.com>
2020-10-15 12:19:31 -0700

    Tell the use how to change permissions if they do not have execute permissions for the scripts

--- app.sh
+++ app.sh
@@ -3,6 +3,37 @@
 source ./scripts/utilitites/color_env_variables.sh
 
 while :; do
+
+	permission=$(stat -L -c "%a" ./scripts/main.sh)
+
+	if [[ $permission -le 664 ]]; then
+		printf "\n You do not have permission to run the scripts."
+
+		printf "\n You can manually set permissions with\n"
+
+		printf "\n ${green}chmod <${blue}who${green}> + <${blue}rwx${green}> ${blue}./path/to/script${end}\n"
+		printf "\n use the chmod command and pass in the users who you want to change permission"
+		printf "\n"
+		printf "\n If you want to change permissions for all files inside sub directories use the -R option"
+		printf "\n u - owner"
+		printf "\n g - group"
+		printf "\n o - others (not user or group permission)"
+		printf "\n a - all"
+		printf "\n"
+		printf "\n then you use the '+' sign followed by the permission"
+		printf "\n r - read"
+		printf "\n w - write"
+		printf "\n x - execute"
+		printf "\n"
+		printf "\n To give all users execute permissions of the scripts use - "
+		printf "\n"
+		printf "\n ${green}chmod -R a+x ./scripts${end}\n"
+
+		printf "\n"
+
+		break
+	fi
+
 	printf "\n Welcome to env setup"
 	printf "\n Type exit to exit the program \n"
 	read input
@@ -12,17 +43,6 @@ while :; do
 		break
 	fi
 
-	permission=$(stat -L -c "%a" ./scripts/main.sh)
-
-	if [[ $permission -le 664 ]]; then
-		printf "\n You do not have permission to run the main script. \n Would you like to give permission to the main script as well as all other subsequent scripts?"
-
-		printf "\n You can manually set permissions with"
-
-		printf "\n ${green}chmod +a+x${end} ${blue}./path/to/script${end}"
-
-	fi
-
-	#./scripts/main.sh $input
+	./scripts/main.sh $input
 
 done
