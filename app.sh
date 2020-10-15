f4a4ce5b8e4d6b623c4333a3bee2b33491d00df4
Raf-Batista <RafaelB2424@gmail.com>
2020-10-15 11:34:40 -0700

    Check file permission of main script to ask a user if they want to give scripts execute permission

--- app.sh
+++ app.sh
@@ -1,19 +1,28 @@
-#!/bin/bash 
+#!/bin/bash
 
 source ./scripts/utilitites/color_env_variables.sh
 
-while : ; do 
-	printf "\n Welcome to env setup" 
+while :; do
+	printf "\n Welcome to env setup"
 	printf "\n Type exit to exit the program \n"
+	read input
 
-	read input 
-
-	if [[ $input == "exit" ]]; then  
+	if [[ $input == "exit" ]]; then
 		echo "Goodbye"
 		break
-	fi 
+	fi
 
+	permission=$(stat -L -c "%a" ./scripts/main.sh)
 
-	./scripts/main.sh $input
+	if [[ $permission -le 664 ]]; then
+		printf "\n You do not have permission to run the main script. \n Would you like to give permission to the main script as well as all other subsequent scripts?"
 
-done 
+		printf "\n You can manually set permissions with"
+
+		printf "\n ${green}chmod +a+x${end} ${blue}./path/to/script${end}"
+
+	fi
+
+	#./scripts/main.sh $input
+
+done
