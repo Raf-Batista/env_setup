bf00d57af4722c1a93f3b2b49c7b687581d73fff
Rafael Batista <RafaelB2424@gmail.com>
2020-10-23 13:26:03 -0400

    Give users info on node

---
+++ scripts/help_scripts/node_help.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+
+printf "\n \n Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine. \n If you are going to work with JavaScript it is probably a good idea to have node installe \n There are some frameworks that rely on having node installed such as Rails and Phoenix \n so it is probably a good idea to have node installed. You also will have the option to install node version manager \n which lets \n you switch between different versions of node and install dependencies for each separate version of node \n \n"
