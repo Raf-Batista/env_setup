a4da49646077001ce23e09824e27dd19321178ef
Rafael Batista <RafaelB2424@gmail.com>
2020-10-23 13:24:36 -0400

    Add comment about the login script

--- scripts/dev_ops/login.rb
+++ scripts/dev_ops/login.rb
@@ -1,8 +1,10 @@
-# use Faraday to login to GitHub
-# require 'faraday'
+# require 'watir'
+# require 'webdrivers'
 
-=begin
-Faraday.post(
-  ...
-)
-=end
+=begin 
+    This script is supposed to sign up to github or login and set up the ssh key. Might scrap this script because this script uses ruby and a user might not set up ruby in their dev environment. This script can work if we clean up Ruby from the system but a simpler solution would be to see if I can write this script usin Bash.
+=end 
+
+
+
+
