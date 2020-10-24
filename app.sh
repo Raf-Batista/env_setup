86ed43bbf8eeaa52e7308262d748a6200d763803
Raf-Batista <RafaelB2424@gmail.com>
2020-10-24 15:00:52 -0700

    Update path to color variables

--- app.sh
+++ app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source ./scripts/utilitites/color_env_variables.sh
+source ./scripts/color_env_variables.sh
 
 while :; do
 
@@ -47,7 +47,7 @@ while :; do
 	fi
 
 	printf "\n Welcome to env setup"
-	printf "\n Type exit to exit the program \n"
+	printf "\n Type exit to exit the program \n \n"
 	read input
 
 	if [[ $input == "exit" ]]; then
