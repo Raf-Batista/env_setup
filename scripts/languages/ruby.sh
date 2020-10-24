9dfa65575073470d14d9b358ccdc505c02c7f161
Raf-Batista <RafaelB2424@gmail.com>
2020-10-24 15:01:48 -0700

    Update ruby script

--- scripts/languages/ruby.sh
+++ scripts/languages/ruby.sh
@@ -20,92 +20,64 @@
 # # change terminal window to login shell
 # echo 'source "/etc/profile.d/rvm.sh"' >> ~/.bashrc
 
-# # install ruby 
+# # install ruby
 
 # rvm install ruby
 
-################ NEW SCRIPT ################# 
+################ NEW SCRIPT #################
 
 # https://github.com/rvm/ubuntu_rvm
 
-# Check if rvm is installed, if it is then we can just install Ruby
-
 # https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" 
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-FILE=$PWD/installed.txt
+FILE=$DIR/installed.txt
 
 
-if test -f "$FILE"; then
+if [[ -f "$FILE" ]]; then
 
-    while read line; do 
+  while read line; do
 
-    if [[ $line == "rvm" ]]; then 
+    if [[ $line == "rvm" ]]; then
       rvm=true
-    fi 
+    fi
 
-    if [[ $line == "ruby" ]]; then 
+    if [[ $line == "ruby" ]]; then
       ruby=true
-    fi 
+    fi
 
   done < $FILE
+
 fi
 
 # https://stackoverflow.com/questions/3601515/how-to-check-if-a-variable-is-set-in-bash
 
+if [ -z ${rvm+x} ]; then
 
-if [ -z ${rvm+x} ]; then 
-  echo "rvm is installed already, would you like to install Ruby?"; 
-else 
-  echo "var is set to '$rvm'"; 
+  echo "You do not have rvm installed"
+
+  sudo apt-get install software-properties-common
+  sudo apt-add-repository -y ppa:rael-gc/rvm
+  sudo apt-get update
+  sudo apt-get install rvm
+
+  echo 'source "/etc/profile.d/rvm.sh"' >>~/.bashrc
+
+  echo "You may need to restart your computer to install Ruby. Run the same script to install it."
+
+  printf "rvm\n" >> $DIR/installed.txt
+
+elif [ -z ${ruby+x} ]; then
+
+  echo "You have rvm installed but not ruby"
+
+  rvm install ruby
+
+  printf "ruby\n" >> $DIR/installed.txt
+
+else
+
+  echo "You have rvm and ruby installed"
+
 fi
-
-
-
-# if test -f "$FILE"; then
-
-#     while read line; do 
-
-#     if [[ $line == "rvm" ]]; then 
-
-#         printf "\nYou have rvm installed already, would you like to install Ruby? \n 'Y/N'\n"
-
-#         read user_input
-
-#         if [[ ${user_input^^} == "YES" || ${input^^} == "Y" ]]; then 
-#           rvm install ruby
-           
-#           echo "ruby" >> $DIR/install.txt
-          
-#           break
-#         fi
-
-#     fi 
-
-#     if [[ $line == "ruby" ]]; then 
-#       echo "You have Ruby installed already"
-#       break
-#     fi 
-
-#     done < FILE
-
-# fi
-
-
-
-# sudo apt-get install software-properties-common
-
-# sudo apt-add-repository -y ppa:rael-gc/rvm
-# sudo apt-get update
-# sudo apt-get install rvm
-
-# echo 'source "/etc/profile.d/rvm.sh"' >> ~/.bashrc
-
-# echo "rvm" >> ../../../../installed.txt
-
-# echo "You may need to restart your computer to install Ruby. Run the same script to install it."
-
-
-
-
