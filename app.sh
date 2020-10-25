fb4b644d0fa46ef296dccc09aea8f79446d58bfd
Raf-Batista <RafaelB2424@gmail.com>
2020-10-25 14:00:01 -0700

    User heredoc to instruct user on changing permissions

--- app.sh
+++ app.sh
@@ -8,30 +8,32 @@ while :; do
 	permission=$(stat -L -c "%a" ./scripts/main.sh)
 
 	if [[ $permission -le 664 ]]; then
-		printf "\n You do not have permission to run the scripts."
+		cat <<- EOF 
 
-		printf "\n You can manually set permissions with\n"
+			You do not have permission to run the scripts
+			You can manually set permissions with
+			${green}chmod <${blue}who${green}> + <${blue}rwx${green}> ${blue}./path/to/script${end}
 
-		printf "\n ${green}chmod <${blue}who${green}> + <${blue}rwx${green}> ${blue}./path/to/script${end}\n"
-		printf "\n use the chmod command and pass in the users who you want to change permission"
-		printf "\n"
-		printf "\n If you want to change permissions for all files inside sub directories use the -R option"
-		printf "\n u - owner"
-		printf "\n g - group"
-		printf "\n o - others (not user or group permission)"
-		printf "\n a - all"
-		printf "\n"
-		printf "\n then you use the '+' sign followed by the permission"
-		printf "\n r - read"
-		printf "\n w - write"
-		printf "\n x - execute"
-		printf "\n"
-		printf "\n To give all users execute permissions of the scripts use - "
-		printf "\n"
-		printf "\n ${green}chmod -R a+x ./scripts${end}\n"
-		printf "\n"
-		printf "\n Do you want env_setup to give all users execute permissions for the scripts? \n"
-		printf " \n"
+			use the chmod command and pass in the users who you want to change permission
+			If you want to change permissions for all files inside sub directories use the -R option
+
+			u - owner"
+			g - group"
+			o - others (not user or group permission)"
+			a - all"
+		
+			then you use the '+' sign followed by the permission"
+			r - read"
+			w - write"
+			x - execute"
+		
+			To give all users execute permissions of the scripts use - 
+	
+			${green}chmod -R a+x ./scripts${end}
+		
+			Do you want env_setup to give all users execute permissions for the scripts? 
+	
+		EOF
 
 		read user_input 
 
