1ddf45adef022abca8f08fb08807515e7974c219
Rafael Batista <RafaelB2424@gmail.com>
2020-10-26 16:53:39 -0400

    Update script

--- scripts/editors/intelliJ.sh
+++ scripts/editors/intelliJ.sh
@@ -1,4 +1,31 @@
  #!/bin/bash
  
- # Install IntelliJ-idea
- sudo snap install intellij-idea-community --classic
+FILE=$DIR/installed.txt
+
+if [[ -f "$FILE" ]]; then
+
+    while read line; do
+
+        if [[ $line == "intelliJ" ]]; then
+            intelliJ=true
+        fi
+
+    done < $FILE
+
+fi
+
+if [[ -z ${surge+x} ]]; then
+
+    echo "You do not have intelliJ installed"
+
+    # Install IntelliJ-idea
+    sudo snap install intellij-idea-community --classic
+
+    printf "intelliJ\n" >> $DIR/installed.txt
+
+else
+
+    echo "You have intelliJ installed"
+
+fi
+
