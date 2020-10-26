0672f97563d35c4251ca69142add9585d60c5cb7
Raf-Batista <RafaelB2424@gmail.com>
2020-10-26 12:37:22 -0700

    Update script

--- scripts/dev_ops/netlify.sh
+++ scripts/dev_ops/netlify.sh
@@ -1,5 +1,31 @@
-#!/bin/bash 
+#!/bin/bash
 
 # https://docs.netlify.com/cli/get-started/
 
-npm install netlify-cli -g
+FILE=$DIR/installed.txt
+
+if [[ -f "$FILE" ]]; then
+
+    while read line; do
+
+        if [[ $line == "netlify" ]]; then
+            netlify=true
+        fi
+
+    done < $FILE
+
+fi
+
+if [[ -z ${netlify+x} ]]; then
+
+    echo "You do not have netlify installed"
+
+    npm install netlify-cli -g
+
+    printf "netlify\n" >>$DIR/installed.txt
+
+else
+
+    echo "You have netlify installed"
+
+fi
