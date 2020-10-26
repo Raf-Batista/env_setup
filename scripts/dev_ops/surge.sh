5769276c358c5d595136b165b3aa233fb9cf889a
Raf-Batista <RafaelB2424@gmail.com>
2020-10-26 12:37:27 -0700

    Update script

--- scripts/dev_ops/surge.sh
+++ scripts/dev_ops/surge.sh
@@ -1,3 +1,31 @@
 #!bin/bash 
 
-npm install --global surge
+FILE=$DIR/installed.txt
+
+if [[ -f "$FILE" ]]; then
+
+    while read line; do
+
+        if [[ $line == "surge" ]]; then
+            surge=true
+        fi
+
+    done < $FILE
+
+fi
+
+if [[ -z ${surge+x} ]]; then
+
+    echo "You do not have surge installed"
+
+    npm install --global surge
+
+    printf "surge\n" >>$DIR/installed.txt
+
+else
+
+    echo "You have surge installed"
+
+fi
+
+
