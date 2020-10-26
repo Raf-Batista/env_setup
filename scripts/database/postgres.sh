210d10f48f44268fdedd84de7226f82ff2cf529b
Raf-Batista <RafaelB2424@gmail.com>
2020-10-26 12:36:58 -0700

    Update script

--- scripts/database/postgres.sh
+++ scripts/database/postgres.sh
@@ -1,18 +1,44 @@
 #!/bin/bash
 
-########## Install Postgresql ############
+FILE=$DIR/installed.txt
 
-sudo apt install postgresql postgresql-contrib
+if [[ -f "$FILE" ]]; then
 
-# set role postgres with password postgres
-sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'postgres';"
+    while read line; do
 
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+        if [[ $line == "postgres" ]]; then
+            postgres=true
+        fi
 
-# Install PGAdmin4
-# Install PostgreSQL key and add repository
+    done <$FILE
 
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+fi
 
-# install PGAdmin repo
-sudo apt install pgadmin4 pgadmin4-apache2 -y
+if [[ -z ${postgres+x} ]]; then
+
+    echo "You do not have surge installed"
+
+    ########## Install Postgresql ############
+
+    sudo apt install postgresql postgresql-contrib
+
+    # set role postgres with password postgres
+    sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'postgres';"
+
+    sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+
+    # Install PGAdmin4
+    # Install PostgreSQL key and add repository
+
+    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+
+    # install PGAdmin repo
+    sudo apt install pgadmin4 pgadmin4-apache2 -y
+
+    printf "postgres\n" >>$DIR/installed.txt
+
+else
+
+    echo "You have surge installed"
+
+fi
