132d2490559d2e0984746073f2870e7b5fd27315
Raf-Batista <RafaelB2424@gmail.com>
2020-10-26 12:37:32 -0700

    Update script

--- scripts/languages/java.sh
+++ scripts/languages/java.sh
@@ -1,53 +1,41 @@
-#/bin/bash
+#!/bin/bash
 
-######## Install Amazon Corretto JDK ##########
+FILE=$DIR/installed.txt
 
- wget -O- https://apt.corretto.aws/corretto.key | sudo apt-key add - 
- sudo add-apt-repository 'deb https://apt.corretto.aws stable main'
+if [[ -f "$FILE" ]]; then
 
-  sudo apt-get update; sudo apt-get install -y java-11-amazon-corretto-jdk
+  while read line; do
+
+    if [[ $line == "java" ]]; then
+      java=true
+    fi
+
+  done < $FILE
+
+fi
+
+if [[ -z ${java+x} ]]; then
+
+  echo "You do not have java installed"
+
+  ######## Install Amazon Corretto JDK ##########
+
+  wget -O- https://apt.corretto.aws/corretto.key | sudo apt-key add -
+  sudo add-apt-repository 'deb https://apt.corretto.aws stable main'
+
+  sudo apt-get update
+  sudo apt-get install -y java-11-amazon-corretto-jdk
 
   sudo apt-get update && sudo apt-get install java-common
 
   wget https://corretto.aws/downloads/latest/amazon-corretto-11-x64-linux-jdk.tar.gz
 
-sudo dpkg --install amazon-corretto-11-x64-linux-jdk.tar.gz
+  sudo dpkg --install amazon-corretto-11-x64-linux-jdk.tar.gz
 
-############# Setup Tomcat Server for Java ################
+  printf "java\n" >>$DIR/installed.txt
 
-# Create user for Tomcat, for security reasons Tomcat should not be run under root user
+else
 
-sudo groupadd tomcat
+  echo "You have java installed"
 
-sudo useradd -s /bin/false –g tomcat –d /opt/tomcat tomcat
-
-cd /tmp
-
-# Tomcat 9
-curl -O http://mirrors.ibiblio.org/apache/tomcat/tomcat-9/v9.0.33/bin/apache-tomcat-9.0.33.tar.gz
-
-# Create directory to extract the tar.gz
-sudo mkdir /opt/tomcat
-
-sudo tar xzvf apache-tomcat-9*tar.gz –C /opt/tomcat –strip-components=1
-
-# Modify Tomcat user permissions
-
-cd /opt/tomcat
-
-# change group ownership
-sudo chgrp –R tomcat /opt/tomcat
-
-# give read access to the conf directory
-sudo chmod –R g+r conf
-
-# change directory permission
-sudo chmod g+x conf
-
-# give tomcat user ownership of webapps work temp and logs directory
-sudo chown –R tomcat webapps/ work temp/ logs
-
-Create system unit file
-
-
-sudo update-java-alternatives -l | grep "/usr.*"
+fi
