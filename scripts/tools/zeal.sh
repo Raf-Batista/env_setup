956f8ac1b944b1db2dff46ee4f56edc5f32fba74
Rafael Batista <RafaelB2424@gmail.com>
2020-10-26 16:54:54 -0400

    Update script

--- scripts/tools/zeal.sh
+++ scripts/tools/zeal.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
 
-####### Install Zeal ######
+FILE=$DIR/installed.txt
 
-sudo add-apt-repository ppa:zeal-developers/ppa
-sudo apt-get update
-sudo apt-get install zeal
+if [[ -f "$FILE" ]]; then
+
+    while read line; do
+
+        if [[ $line == "zeal" ]]; then
+            zeal=true
+        fi
+
+    done < $FILE
+
+fi
+
+if [[ -z ${zeal+x} ]]; then
+
+    echo "You do not have zeal installed"
+
+    ####### Install Zeal #######
+
+    sudo add-apt-repository ppa:zeal-developers/ppa
+    sudo apt-get update
+    sudo apt-get install zeal
+
+    printf "zeal\n" >> $DIR/installed.txt
+
+else
+
+    echo "You have zeal installed"
+
+fi
+
