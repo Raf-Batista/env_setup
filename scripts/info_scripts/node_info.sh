a4cf46abc4b8c0a7368d93099b216aac2e0b257a
Rafael Batista <RafaelB2424@gmail.com>
2020-10-27 21:48:32 -0400

    Change node info to use heredoc

---
+++ scripts/info_scripts/node_info.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+cat <<- EOF 
+
+Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine. If you are going to work with JavaScript it is probably a good idea to have node installe There are some frameworks that rely on having node installed such as Rails and Phoenix so it is probably a good idea to have node installed. You also will have the option to install node version manager which lets you switch between different versions of node and install dependencies for each separate version of node 
+
+EOF
