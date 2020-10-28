805f8c01f4e3865056bd7afd55ca2423830aba47
Raf-Batista <RafaelB2424@gmail.com>
2020-10-27 19:23:00 -0700

    Add rails info script

---
+++ scripts/info_scripts/rails_info.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+
+cat <<- EOF 
+
+Ruby on Rails is a server-side web application which implements the server side MVC pattern. It is written in Ruby which offers the same easy to read syntax you would expect working with Ruby.
+EOF
