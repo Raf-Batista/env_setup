899ab6cbabfaeeb17d875b8207263629bb3efa99
Rafael Batista <RafaelB2424@gmail.com>
2020-10-27 21:49:48 -0400

    Add ruby info script

---
+++ scripts/info_scripts/ruby_info.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+
+cat <<- EOF 
+
+Ruby is a dynamic, open source programming object oriented programming language with easy to read syntax and a focus on simplicity and productivity. There are different frameworks for creating web
+applications such as Sinatra and Ruby on Rails.
+
+EOF
