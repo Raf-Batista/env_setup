38f0c30dfba1b47ca4c03e9b245b0c87609f607b
Raf-Batista <RafaelB2424@gmail.com>
2020-10-27 19:23:10 -0700

    Update script

--- scripts/info_scripts/phoenix_info.sh
+++ scripts/info_scripts/phoenix_info.sh
@@ -2,6 +2,6 @@
 
 cat <<- EOF 
 
-Phoenix is a a web framework written which implements the server side MVC pattern. Phoenix will feel familiar if you have experience in Ruby on Rails or Django. Phoenix is written in Elixir which is a functional programming language. If you want to build web applications in something other than OOP then Phoenix may be a good choice.
+Phoenix is a a web framework which implements the server side MVC pattern. Phoenix will feel familiar if you have experience in Ruby on Rails or Django. Phoenix is written in Elixir which is a functional programming language. If you want to build web applications in something other than OOP then Phoenix may be a good choice.
 
 EOF
