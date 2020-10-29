7f231821e793c31babb632b06b1faacd1cf1243f
Rafael Batista <RafaelB2424@gmail.com>
2020-10-29 19:29:42 -0400

    Create java info script

---
+++ scripts/info_scripts/java_info.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+cat <<- EOF 
+
+Java is an object oriented programming language and is arguably one of the most popular programming languages. From desktop, to mobile, to web, Java can do it all.
+
+EOF
