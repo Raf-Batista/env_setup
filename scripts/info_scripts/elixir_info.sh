e5d499f49d0011cb4eff80fc2e1561ce7f14ced4
Rafael Batista <RafaelB2424@gmail.com>
2020-10-29 19:29:29 -0400

    Create elixir info script

---
+++ scripts/info_scripts/elixir_info.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+cat <<- EOF 
+
+Elixir is a dynamic, functional language designed for building scalable and maintainable applications. Elixir uses the Erlang VM which is known for running low-latency, distributed and fault-tolerant systems. If you want to use a functional programming language with Ruby like syntax then you might like Elixir     
+
+EOF
