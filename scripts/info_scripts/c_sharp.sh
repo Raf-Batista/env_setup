24663c13017ecefa0fff160718d14d38da4c72ad
Rafael Batista <RafaelB2424@gmail.com>
2020-10-29 19:29:12 -0400

    Create c sharp info script

---
+++ scripts/info_scripts/c_sharp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+cat <<- EOF 
+
+C# is a general-purpose object oriented programming language. If you are familiar with C, C++, Java or JavaScript then the curly brace syntax will be familiar to you. You can make different kinds of applications from web using ASP.Net to native Windows Desktop application.
+    
+EOF
