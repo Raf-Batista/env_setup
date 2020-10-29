4e2718abb5663fd533d3ced62b25857b1da6c118
Rafael Batista <RafaelB2424@gmail.com>
2020-10-29 19:30:32 -0400

    Create django info script

---
+++ scripts/info_scripts/django_info.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+cat <<- EOF 
+
+Django is a high level web framework written in Python. Django is the Python equivalent of Ruby on Rails. If you want to build large production ready web applications in Python then look no further than Django.
+    
+EOF
