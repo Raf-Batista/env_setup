99886910ecdaf6b5aa53330507a5dd21e2767587
Rafael Batista <RafaelB2424@gmail.com>
2020-10-29 19:30:08 -0400

    Create sinatra info script

---
+++ scripts/info_scripts/sinastra.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+cat <<- EOF 
+
+Sinatra is a lightweight framework for quickly creating web applications in Ruby. If you want to make a small web applications in Ruby then Sinatra might be a great choice compared to Ruby on Rails.
+
+EOF
