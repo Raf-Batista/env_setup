6afe9b80091132db27824fe3e631827969d8501c
Raf-Batista <RafaelB2424@gmail.com>
2020-11-20 19:54:22 -0800

    Create script to be able to upload to google drive using terminal

---
+++ scripts/tools/google_drive_upload.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+
+# https://olivermarshall.net/how-to-upload-a-file-to-google-drive-from-the-command-line/
+
+# Use Google Drive to upload the compressed file system 
+
+wget https://docs.google.com/uc?id=0B3X9GlR6EmbnWksyTEtCM0VfaFE&export=download
+
+mv uc\?id\=0B3X9GlR6EmbnWksyTEtCM0VfaFE gdrive
+
+chmod +x gdrive
+	
+sudo install gdrive /usr/local/bin/gdrive
+
+gdrive list
+
+gdrive upload backups.tar.gz
+
+printf "gdrive\n" >> $DIR/installed.txt
